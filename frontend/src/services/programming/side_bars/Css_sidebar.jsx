a5e783d39f3b3babbb8e5aefdbe9e1984290772f
malikumair687 <malikumairbwp786@gmail.com>
2025-06-23 10:48:18 +0500

    css sidebar

--- frontend/src/services/programming/side_bars/Css_sidebar.jsx
+++ frontend/src/services/programming/side_bars/Css_sidebar.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+const TutorialArray=[
+  {id:1,topic:"CSS HOME",link:"/"},
+  {id:2,topic:"CSS Introduction",link:"/"},
+  {id:3,topic:"CSS Syntax",link:"/"},
+  {id:4,topic:"CSS HOME",link:"/"},
+  {id:5,topic:"CSS HOME",link:"/"},
+]
 const Css_sidebar = () => {
   return (
     <div className="w-[20vw] p-3 border overflow-y-scroll h-screen pb-20 bg-gray-800 text-white">
